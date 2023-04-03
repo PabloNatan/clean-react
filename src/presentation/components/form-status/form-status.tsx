15110e771f586357d115e08d8424da64b0b70965
Pablo Santos <pablo.1999@alunos.utfpr.edu.br>
2023-04-03 09:44:25 -0300

    feat: ensure login will not load spinner and error on start

--- src/presentation/components/form-status/form-status.tsx
+++ src/presentation/components/form-status/form-status.tsx
@@ -1,12 +1,18 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import Styles from './form-status.styles.scss'
 import { Spinner } from '../spinner'
+import { FormContext } from '@/presentation/contexts/form/form-context'
 
 export const FormStatus: React.FC = () => {
+  const { isLoading, errorMessage } = useContext(FormContext)
   return (
-    <div className={Styles.errorWrapper}>
-      <Spinner className={Styles.spinner} />
-      <span className={Styles.error}>Erro</span>
+    <div className={Styles.errorWrapper} role="status">
+      {isLoading && <Spinner className={Styles.spinner} aria-label="spinner" />}
+      {errorMessage && (
+        <span className={Styles.error} aria-label="error-message">
+          {errorMessage}
+        </span>
+      )}
     </div>
   )
 }
--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
-import { render } from '@testing-library/react'
+import { render, screen, within } from '@testing-library/react'
 import { Login } from './login'
 
 describe('Login Component', () => {
-  test('', () => {
+  test('Should not render spinner and error on start', () => {
     render(<Login />)
+    const formStatus = screen.getByRole('status')
+    const spinner = within(formStatus).queryByLabelText('spinner')
+    const errorMessage = within(formStatus).queryByLabelText('error-message')
+    expect(spinner).not.toBeInTheDocument()
+    expect(errorMessage).not.toBeInTheDocument()
   })
 })
