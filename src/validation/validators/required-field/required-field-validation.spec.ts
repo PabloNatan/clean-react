17dad856c694554d7abcfe3a8ebd3cf271668136
Pablo Santos <pablo.1999@alunos.utfpr.edu.br>
2023-04-11 05:22:04 -0300

    refactor: move required field validation to a different folder

--- src/validation/validators/required-field/required-field-validation.spec.ts
+++ src/validation/validators/required-field/required-field-validation.spec.ts
@@ -1,4 +1,4 @@
-import { RequiredFieldValidation } from '@/validation/required-field/required-field-validation'
+import { RequiredFieldValidation } from '@/validation/validators/required-field/required-field-validation'
 import faker from 'faker'
 import { RequiredFieldError } from '@/validation/errors/required-field-error'
 
--- src/validation/validators/required-field/required-field-validation.ts
+++ src/validation/validators/required-field/required-field-validation.ts
@@ -1,5 +1,5 @@
 import { RequiredFieldError } from '@/validation/errors/required-field-error'
-import { type FieldValidation } from '../protocols/field-validation'
+import { type FieldValidation } from '@/validation/protocols/field-validation'
 
 export class RequiredFieldValidation implements FieldValidation {
   constructor(readonly field: string) {}
