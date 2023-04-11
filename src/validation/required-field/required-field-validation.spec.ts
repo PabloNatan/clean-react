d4b59a7ca8013cffa2c416afff5d7837f379ca0a
Pablo Santos <pablo.1999@alunos.utfpr.edu.br>
2023-04-11 05:10:41 -0300

    feat: ensure RequiredFieldValidation return falsy  if field is not  empty

--- src/validation/required-field/required-field-validation.spec.ts
+++ src/validation/required-field/required-field-validation.spec.ts
@@ -1,8 +1,16 @@
 import { RequiredFieldValidation } from '@/validation/required-field/required-field-validation'
+import faker from 'faker'
+import { RequiredFieldError } from '../errors/required-field-error'
 
 describe('RequiredFieldValidation', () => {
   test('Should return error if field is empty', () => {
     const sut = new RequiredFieldValidation('email')
-    sut.validate('')
+    const error = sut.validate('')
+    expect(error).toEqual(new RequiredFieldError())
+  })
+  test('Should return falsy if field is not empty', () => {
+    const sut = new RequiredFieldValidation('email')
+    const error = sut.validate(faker.random.word())
+    expect(error).toBeFalsy()
   })
 })
