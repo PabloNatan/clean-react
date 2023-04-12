166546f33739020f82cffaed512d71df8ef3ec5a
Pablo Santos <pablo.1999@alunos.utfpr.edu.br>
2023-04-12 05:01:51 -0300

    feat: ensure ValidationBuilder has an email  helper

--- src/validation/validators/builder/validation-builder.spec.ts
+++ src/validation/validators/builder/validation-builder.spec.ts
@@ -1,4 +1,5 @@
 import {
+  EmailValidation,
   RequiredFieldValidation,
   ValidationBuilder
 } from '@/validation/validators'
@@ -7,5 +8,9 @@ describe('ValidationBuilder', () => {
   test('Should return RequiredFieldValidation', () => {
     const validations = ValidationBuilder.field('any_field').required().build()
     expect(validations).toEqual([new RequiredFieldValidation('any_field')])
+  })
+  test('Should return EmailValidation', () => {
+    const validations = ValidationBuilder.field('any_field').email().build()
+    expect(validations).toEqual([new EmailValidation('any_field')])
   })
 })
