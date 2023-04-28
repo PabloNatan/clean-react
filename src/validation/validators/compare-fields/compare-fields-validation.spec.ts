76bb7356f02a0a9c37d7dc7f3675fa5390a13579
Pablo Santos <pablo.1999@alunos.utfpr.edu.br>
2023-04-28 03:46:03 -0300

    feat: ensure CompareFieldsValidation returns falsy if validation succeds

--- src/validation/validators/compare-fields/compare-fields-validation.spec.ts
+++ src/validation/validators/compare-fields/compare-fields-validation.spec.ts
@@ -14,4 +14,12 @@ describe('CompareFieldsValidation', () => {
     const error = sut.validate(faker.random.word())
     expect(error).toEqual(new InvalidFieldError(field))
   })
+
+  test('Should return falsy if compare is valid', () => {
+    const valueToCompare = faker.random.word()
+    const field = faker.database.column()
+    const sut = makeSut(field, valueToCompare)
+    const error = sut.validate(valueToCompare)
+    expect(error).toBeFalsy()
+  })
 })
