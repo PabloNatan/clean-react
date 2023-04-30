7689aa6ac0cae0b449da7bdf3bb5f287bad74abd
Pablo Santos <pablo.1999@alunos.utfpr.edu.br>
2023-04-30 04:18:06 -0300

    chore: add cypress

---
+++ cypress.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from 'cypress'
+
+export default defineConfig({
+  fileServerFolder: 'src/main/test/cypress',
+  fixturesFolder: 'src/main/test/cypres/fixtures',
+  screenshotsFolder: 'src/main/test/cypres/screenshots',
+  videosFolder: 'src/main/test/cypres/videos',
+  downloadsFolder: 'src/main/test/cypres/downloads',
+
+  e2e: {
+    specPattern: 'src/main/test/cypress/**/**/*.spec.{js,jsx,ts,tsx}',
+    supportFile: false,
+    fileServerFolder: 'src/main/test/cypress',
+    baseUrl: 'http://localhost:8080'
+  }
+})
--- jest.config.js
+++ jest.config.js
@@ -14,7 +14,7 @@ module.exports = {
   setupFilesAfterEnv: ['<rootDir>/src/main/config/jest-setup.ts'],
   testPathIgnorePatterns: [
     '<rootDir>/node_modules/',
-    '<rootDir>/tests/e2e/cypress'
+    '<rootDir>/src/main/test/cypress'
   ],
   testEnvironment: 'jsdom',
   transform: {
