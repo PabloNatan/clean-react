cd383d7f582270841b5153afd1cd9cbfe9ef47e5
Pablo Santos <pablo.1999@alunos.utfpr.edu.br>
2023-05-01 06:28:30 -0300

    test: ensure SignUp present error if form is invalid

--- src/main/test/cypress/e2e/signup.spec.ts
+++ src/main/test/cypress/e2e/signup.spec.ts
@@ -1,3 +1,4 @@
+import { faker } from '@faker-js/faker'
 import * as FormHelper from '../utils/form-helper'
 
 describe('SignUp', () => {
@@ -9,8 +10,19 @@ describe('SignUp', () => {
     FormHelper.testInputStatus('name', 'Campo obrigatório')
     FormHelper.testInputStatus('email', 'Campo obrigatório')
     FormHelper.testInputStatus('password', 'Campo obrigatório')
-    FormHelper.testInputStatus('passowrdConfirmation', 'Campo obrigatório')
+    FormHelper.testInputStatus('passwordConfirmation', 'Campo obrigatório')
     cy.get('button[type=submit]').should('have.attr', 'disabled')
     cy.getByLabel('request-feedback').should('not.have.descendants')
+  })
+
+  it('Should present error state if form is invalid', () => {
+    cy.typeByLabel('name', faker.random.alpha(3))
+    FormHelper.testInputStatus('name', 'Campo inválido')
+    cy.typeByLabel('email', faker.random.words())
+    FormHelper.testInputStatus('email', 'Campo inválido')
+    cy.typeByLabel('password', faker.random.alphaNumeric(3))
+    FormHelper.testInputStatus('password', 'Campo inválido')
+    cy.typeByLabel('passwordConfirmation', faker.random.alphaNumeric(4))
+    FormHelper.testInputStatus('passwordConfirmation', 'Campo inválido')
   })
 })
