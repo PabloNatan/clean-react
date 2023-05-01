0efc6ecc1a9390fc4859fac5bb8afce848aefbcc
Pablo Santos <pablo.1999@alunos.utfpr.edu.br>
2023-05-01 06:49:00 -0300

    test: ensure Signup present UnexpectedError return invalid data

--- src/main/test/cypress/e2e/signup.spec.ts
+++ src/main/test/cypress/e2e/signup.spec.ts
@@ -67,4 +67,13 @@ describe('SignUp', () => {
     )
     FormHelper.testUrl('/signup')
   })
+
+  it('Should present UnexpectedError if invalid data is return', () => {
+    Http.mockInvalidData()
+    simulateValidSubmit()
+    FormHelper.testMainError(
+      'Algo de errado aconteceu. Tente novamente em breve'
+    )
+    FormHelper.testUrl('/signup')
+  })
 })
