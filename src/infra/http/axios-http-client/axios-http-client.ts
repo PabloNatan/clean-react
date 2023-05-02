2342588bd36fc5006879798bf5fdfe1fac77542e
Pablo Santos <pablo.1999@alunos.utfpr.edu.br>
2023-05-02 05:08:47 -0300

    refactor: move duplicated code to a helper

--- src/infra/http/axios-http-client/axios-http-client.ts
+++ src/infra/http/axios-http-client/axios-http-client.ts
@@ -7,30 +7,30 @@ import {
 } from '@/data/protocols/http'
 
 export class AxiosHttpClient implements HttpPostClient {
-  async post(params: HttpPostParams): Promise<HttpResponse> {
-    let axiosResponse: AxiosResponse<any>
-    try {
-      axiosResponse = await axios.post(params.url, params.body)
-    } catch (error) {
-      axiosResponse = error.response
-    }
+  private adapt(axiosResponse: AxiosResponse): HttpResponse {
     return {
       statusCode: axiosResponse.status,
       body: axiosResponse.data
     }
   }
 
+  async post(params: HttpPostParams): Promise<HttpResponse> {
+    let axiosResponse: AxiosResponse
+    try {
+      axiosResponse = await axios.post(params.url, params.body)
+    } catch (error) {
+      axiosResponse = error.response
+    }
+    return this.adapt(axiosResponse)
+  }
+
   async get(params: HttpGetParams): Promise<HttpResponse> {
-    let axiosResponse: AxiosResponse<any>
+    let axiosResponse: AxiosResponse
     try {
       axiosResponse = await axios.get(params.url)
     } catch (error) {
       axiosResponse = error.response
     }
-
-    return {
-      statusCode: axiosResponse.status,
-      body: axiosResponse.data
-    }
+    return this.adapt(axiosResponse)
   }
 }
