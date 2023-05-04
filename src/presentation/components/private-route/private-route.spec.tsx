e4fbce30f2922d1b2b1ef23b7251b6337d886fa8
Pablo Santos <pablo.1999@alunos.utfpr.edu.br>
2023-05-04 03:51:48 -0300

    feat: ensure PrivateRoute renders components if token is not empty

--- src/presentation/components/private-route/private-route.spec.tsx
+++ src/presentation/components/private-route/private-route.spec.tsx
@@ -1,28 +1,48 @@
-import React from 'react'
-import { PrivateRoute } from './private-route'
+import { mockAccountModel } from '@/domain/test'
+import { ApiContext } from '@/presentation/contexts'
 import { render } from '@testing-library/react'
+import React from 'react'
 import { RouterProvider, createMemoryRouter } from 'react-router-dom'
+import { PrivateRoute } from './private-route'
+
+type SutTypes = {
+  history: ReturnType<typeof createMemoryRouter>
+}
+
+const makeSut = (account = mockAccountModel()): SutTypes => {
+  const history = createMemoryRouter(
+    [
+      {
+        path: '/',
+        element: <PrivateRoute />
+      },
+      {
+        path: '/login',
+        element: <></>
+      }
+    ],
+    {
+      initialIndex: 0,
+      initialEntries: ['/']
+    }
+  )
+
+  render(
+    <ApiContext.Provider value={{ getCurrentAccount: () => account }}>
+      <RouterProvider router={history} />
+    </ApiContext.Provider>
+  )
+  return { history }
+}
 
 describe('PrivateRoute', () => {
   test('Should redirect to /login if token is empty', () => {
-    const element = <PrivateRoute />
-    const history = createMemoryRouter(
-      [
-        {
-          path: '/',
-          element
-        },
-        {
-          path: '/login',
-          element: <></>
-        }
-      ],
-      {
-        initialIndex: 0,
-        initialEntries: ['/']
-      }
-    )
-    render(<RouterProvider router={history} />)
+    const { history } = makeSut(null)
     expect(history.state.location.pathname).toBe('/login')
+  })
+
+  test('Should render current component if token is not empty', () => {
+    const { history } = makeSut()
+    expect(history.state.location.pathname).toBe('/')
   })
 })
--- src/presentation/components/private-route/private-route.tsx
+++ src/presentation/components/private-route/private-route.tsx
@@ -1,6 +1,13 @@
+/* eslint-disable multiline-ternary */
+import { useApiContext } from '@/presentation/contexts'
 import React from 'react'
-import { Navigate } from 'react-router-dom'
+import { Navigate, Route, type RouteProps } from 'react-router-dom'
 
-export const PrivateRoute: React.FC = () => {
-  return <Navigate to="/login" />
+export const PrivateRoute: React.FC<RouteProps> = (props: RouteProps) => {
+  const { getCurrentAccount } = useApiContext()
+  return getCurrentAccount()?.accessToken ? (
+    <Route {...props} />
+  ) : (
+    <Navigate to="/login" />
+  )
 }
