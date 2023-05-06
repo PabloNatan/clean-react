42e3c33d66c8991165dada9a81a29f4718913259
Pablo Santos <pablo.1999@alunos.utfpr.edu.br>
2023-05-06 06:07:53 -0300

    feat: add AuthorizeHttpGetClientDecorator factory

--- src/main/factories/usecases/load-survey-list/remote-load-survey-list-factory.ts
+++ src/main/factories/usecases/load-survey-list/remote-load-survey-list-factory.ts
@@ -1,7 +1,11 @@
+import { makeApiUrl } from '@/main/factories/http'
+import { makeAuthorizeHttpGetClientDecorator } from '@/main/factories/decorators'
 import { RemoteLoadSurveyList } from '@/data/usecases/load-survey-list/load-survey-list'
 import { type LoadSurveyList } from '@/domain/usecases'
-import { makeApiUrl, makeAxiosHttpClient } from '../../http'
 
 export const makeRemoteLoadSurveyList = (): LoadSurveyList => {
-  return new RemoteLoadSurveyList(makeApiUrl('surveys'), makeAxiosHttpClient())
+  return new RemoteLoadSurveyList(
+    makeApiUrl('surveys'),
+    makeAuthorizeHttpGetClientDecorator()
+  )
 }
