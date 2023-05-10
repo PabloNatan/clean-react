5247a6520aec577104b567e9668a1a6fd349e91a
Pablo Santos <pablo.1999@alunos.utfpr.edu.br>
2023-05-10 04:42:12 -0300

    refactor: stop cypress:run from generate video

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,10 +4,10 @@ export default defineConfig({
   fileServerFolder: 'src/main/test/cypress',
   fixturesFolder: 'src/main/test/cypress/fixtures',
   screenshotsFolder: 'src/main/test/cypress/screenshots',
-  videosFolder: 'src/main/test/cypress/videos',
   downloadsFolder: 'src/main/test/cypress/downloads',
 
   e2e: {
+    video: false,
     specPattern: 'src/main/test/cypress/**/**/*.spec.{js,jsx,ts,tsx}',
     supportFile: 'src/main/test/cypress/support',
     fileServerFolder: 'src/main/test/cypress',
